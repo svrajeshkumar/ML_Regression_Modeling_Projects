bcdd68a40c9f7c0587606f2edc85ba6dec7a932e
svrajeshkumar <sv_rajesh_kumar@outlook.com>
2023-01-11 18:05:18 +0530

    Updated Accuracy Score
    Updated Accuracy Score

--- Classification_Model/Classification.py
+++ Classification_Model/Classification.py
@@ -2,6 +2,8 @@ import sklearn
 from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
+
 
 # Load dataset
 data = load_breast_cancer()
@@ -13,10 +15,10 @@ feature_names = data['feature_names']
 features = data['data']
 
 # Look at our data
-print(label_names)
-print(labels[0])
-print(feature_names[0])
-print(features[0])
+#print(label_names)
+#print(labels[0])
+#print(feature_names[0])
+#print(features[0])
 
 # Split our data
 train, test, train_labels, test_labels = train_test_split(features,labels, test_size=0.33,random_state=42)
@@ -29,3 +31,6 @@ model = gnb.fit(train, train_labels)
 # Make predictions
 preds = gnb.predict(test)
 print(preds)
+
+# Evaluate accuracy
+print(accuracy_score(test_labels, preds))
