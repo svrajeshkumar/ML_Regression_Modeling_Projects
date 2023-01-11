31d61cfa935b11e6fe69d46961322cf0a5bba410
svrajeshkumar <sv_rajesh_kumar@outlook.com>
2023-01-11 18:00:44 +0530

    Classification Modelling
    Classification Modelling

---
+++ Classification_Model/Classification.py
@@ -0,0 +1,11 @@
+import sklearn
+from sklearn.datasets import load_breast_cancer
+
+# Load dataset
+data = load_breast_cancer()
+
+
+label_names = data['target_names']
+labels = data['target']
+feature_names = data['feature_names']
+features = data['data']
